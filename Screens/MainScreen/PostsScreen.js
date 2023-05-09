31e51a6361edb7cfe9ed57d2ee26652853934cd7
Inna Kukla <0969125133@ukr.net>
2023-05-09 15:45:59 +0300

    Update PostsScreen.js

--- Screens/MainScreen/PostsScreen.js
+++ Screens/MainScreen/PostsScreen.js
@@ -1,8 +1,8 @@
 import React from "react";
 import { moduleName } from "react-native";
 import { createStackNavigator } from "@react-navigation/stack";
-import {Home} from "../nestedScreens/Home";
-import {CommentsScreen} from '../nestedScreens/CommentsScreen'
+import { Home } from "../nestedScreens/Home";
+import { CommentsScreen } from "../nestedScreens/CommentsScreen";
 import { MapScreen } from "../nestedScreens/MapScreen";
 
 const NestedScreen = createStackNavigator();
@@ -10,9 +10,7 @@ const NestedScreen = createStackNavigator();
 export const PostsScreen = () => {
   return (
     <NestedScreen.Navigator>
-      <NestedScreen.Screen  tabBarOptions={{
-        showLabel: false,
-      }} name="Home" component={Home} />
+      <NestedScreen.Screen name="Home" component={Home} />
       <NestedScreen.Screen name="CommentsScreen" component={CommentsScreen} />
       <NestedScreen.Screen name="MapScreen" component={MapScreen} />
     </NestedScreen.Navigator>
