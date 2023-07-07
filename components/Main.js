bf1b5a5851b3c09070723bc3ca3774315d60aa3b
Inna Kukla <0969125133@ukr.net>
2023-07-07 12:36:28 +0300

    Create Main.js

---
+++ components/Main.js
@@ -0,0 +1,19 @@
+import React, { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { useRoute } from "../router";
+import { NavigationContainer } from "@react-navigation/native";
+import { authStateChangeUser } from "../redux/auth/authOperation";
+
+export const Main = () => {
+
+  const {stateChange} = useSelector((state) => state.auth);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(authStateChangeUser())
+  }, [])
+
+  const routing = useRoute(stateChange);
+
+  return <NavigationContainer>{routing}</NavigationContainer>;
+}
