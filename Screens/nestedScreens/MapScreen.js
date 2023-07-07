80c4c5aaba4a8fbd89560f84b6a50820145630e4
Inna Kukla <0969125133@ukr.net>
2023-07-07 12:37:15 +0300

    Update MapScreen.js

--- Screens/nestedScreens/MapScreen.js
+++ Screens/nestedScreens/MapScreen.js
@@ -2,19 +2,21 @@ import React from "react";
 import { Text, View, StyleSheet } from "react-native";
 import MapView, {Marker} from "react-native-maps";
 
-export const MapScreen = () => {
+export const MapScreen = ({ route }) => {
+  console.log("route.params.location", route);
+  const { latitude, longitude } = route.params.location;
   return (
     <View style={styles.container}>
       <MapView
       style={{flex: 1}}
         initialRegion={{
-          latitude: 37.78825,
-          longitude: -122.4324,
-          latitudeDelta: 0.0922,
-          longitudeDelta: 0.0421,
+          latitude: latitude,
+          longitude: longitude,
+          latitudeDelta: 0.00922,
+          longitudeDelta: 0.006,
         }}
       >
-        <Marker coordinate={{latitude: 37.78825, longitude: -122.4324}}/>
+        <Marker coordinate={{latitude, longitude}}/>
       </MapView>
     </View>
   );
