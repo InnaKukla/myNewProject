2b03425d4a7ccbe7cdbbd38bf6d51fcf02eafa8d
Inna Kukla <0969125133@ukr.net>
2023-07-07 12:37:05 +0300

    Update PostsScreen.js

--- Screens/MainScreen/PostsScreen.js
+++ Screens/MainScreen/PostsScreen.js
@@ -1,5 +1,5 @@
+import 'react-native-gesture-handler';
 import React from "react";
-import { moduleName } from "react-native";
 import { createStackNavigator } from "@react-navigation/stack";
 import { Home } from "../nestedScreens/Home";
 import { CommentsScreen } from "../nestedScreens/CommentsScreen";
@@ -9,10 +9,10 @@ const NestedScreen = createStackNavigator();
 
 export const PostsScreen = () => {
   return (
-    <NestedScreen.Navigator>
-      <NestedScreen.Screen name="Home" component={Home} />
-      <NestedScreen.Screen name="CommentsScreen" component={CommentsScreen} />
-      <NestedScreen.Screen name="MapScreen" component={MapScreen} />
+    <NestedScreen.Navigator >
+      <NestedScreen.Screen options={{headerShown: false}} name="Home" component={Home} />
+      <NestedScreen.Screen options={{headerBackTitleVisible: false }} name="Коментарі" component={CommentsScreen} />
+      <NestedScreen.Screen options={{headerBackTitleVisible: false}} name="Карта" component={MapScreen} />
     </NestedScreen.Navigator>
   );
 };
